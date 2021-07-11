7384664b6d954a2aac7613904e1a3dc205624988
Darko Vujinovic <vujinovicdarko0@gmail.com>
2021-07-11 14:39:49 +0200

    Check LocalMachine registry key

--- WorkpulsCamera/WorkpulsCamera.cs
+++ WorkpulsCamera/WorkpulsCamera.cs
@@ -60,6 +60,54 @@ namespace WorkpulsCamera
                 }                
             }
 
+            using (var key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\webcam\NonPackaged"))
+            {
+                if (key != null)
+                {
+                    foreach (var subKeyName in key.GetSubKeyNames())
+                    {
+                        using (var subKey = key.OpenSubKey(subKeyName))
+                        {
+                            if (subKey.GetValueNames().Contains("LastUsedTimeStop"))
+                            {
+                                var endTime = subKey.GetValue("LastUsedTimeStop") is long ? (long)subKey.GetValue("LastUsedTimeStop") : -1;
+                                if (endTime <= 0)
+                                {
+                                    isAvailable = true;
+                                    String UserName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+                                    string micUsage = "Web Camera is currently in use. | Time: " + DateTime.Now.ToString() + " | User: " + UserName + " |";
+                                    Console.WriteLine(micUsage);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            using (var key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\webcam"))
+            {
+                if (key != null)
+                {
+                    foreach (var subKeyName in key.GetSubKeyNames())
+                    {
+                        using (var subKey = key.OpenSubKey(subKeyName))
+                        {
+                            if (subKey.GetValueNames().Contains("LastUsedTimeStop"))
+                            {
+                                var endTime = subKey.GetValue("LastUsedTimeStop") is long ? (long)subKey.GetValue("LastUsedTimeStop") : -1;
+                                if (endTime <= 0)
+                                {
+                                    isAvailable = true;
+                                    String UserName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+                                    string micUsage = "Web Camera is currently in use. | Time: " + DateTime.Now.ToString() + " | User: " + UserName + " |";
+                                    Console.WriteLine(micUsage);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
             // if is not available at all
             if (isAvailable == false)
             {
--- WorkpulsMicrophone/WorkpulsMicrophone.cs
+++ WorkpulsMicrophone/WorkpulsMicrophone.cs
@@ -60,6 +60,54 @@ namespace WorkpulsMicrophone
                 }                
             }
 
+            using (var key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\microphone\NonPackaged"))
+            {
+                if (key != null)
+                {
+                    foreach (var subKeyName in key.GetSubKeyNames())
+                    {
+                        using (var subKey = key.OpenSubKey(subKeyName))
+                        {
+                            if (subKey.GetValueNames().Contains("LastUsedTimeStop"))
+                            {
+                                var endTime = subKey.GetValue("LastUsedTimeStop") is long ? (long)subKey.GetValue("LastUsedTimeStop") : -1;
+                                if (endTime <= 0)
+                                {
+                                    isAvailable = true;
+                                    String UserName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+                                    string micUsage = "Microphone is currently in use. | Time: " + DateTime.Now.ToString() + " | User: " + UserName + " |";
+                                    Console.WriteLine(micUsage);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            using (var key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\microphone"))
+            {
+                if (key != null)
+                {
+                    foreach (var subKeyName in key.GetSubKeyNames())
+                    {
+                        using (var subKey = key.OpenSubKey(subKeyName))
+                        {
+                            if (subKey.GetValueNames().Contains("LastUsedTimeStop"))
+                            {
+                                var endTime = subKey.GetValue("LastUsedTimeStop") is long ? (long)subKey.GetValue("LastUsedTimeStop") : -1;
+                                if (endTime <= 0)
+                                {
+                                    isAvailable = true;
+                                    String UserName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+                                    string micUsage = "Microphone is currently in use. | Time: " + DateTime.Now.ToString() + " | User: " + UserName + " |";
+                                    Console.WriteLine(micUsage);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
             // if is not available at all
             if(isAvailable == false)
             {
